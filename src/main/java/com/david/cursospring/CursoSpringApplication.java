6fb7ff3fd52238eefd7e4e6f8fe8229f4f658b1c
DavidLuna0 <ddavidr569@gmail.com>
2020-09-22 22:52:05 -0300

    [Update] - Adding debug objects to the database

--- src/main/java/com/david/cursospring/CursoSpringApplication.java
+++ src/main/java/com/david/cursospring/CursoSpringApplication.java
@@ -1,13 +1,34 @@
 package com.david.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.david.cursospring.domain.Categoria;
+import com.david.cursospring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoSpringApplication {
+public class CursoSpringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
 	}
 
 }
