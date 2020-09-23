49ad88f6c6857ce392c2225c93d9dd98e6ab3af6
DavidLuna0 <ddavidr569@gmail.com>
2020-09-23 11:38:19 -0300

    [Update] - Adding ObjectNotFoundException to the CategoriaService

--- src/main/java/com/david/cursospring/services/CategoriaService.java
+++ src/main/java/com/david/cursospring/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.david.cursospring.domain.Categoria;
 import com.david.cursospring.repositories.CategoriaRepository;
+import com.david.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! - Id: " + id + " Tipo: " + Categoria.class.getName()));
 	}
 }
