3f820b58673a2c9b2c705f85581d30ca51ab5ca7
DavidLuna0 <ddavidr569@gmail.com>
2020-09-23 11:15:26 -0300

    [Update] - Adding new objects to debug and testing

--- src/main/java/com/david/cursospring/CursoSpringApplication.java
+++ src/main/java/com/david/cursospring/CursoSpringApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.david.cursospring.domain.Categoria;
+import com.david.cursospring.domain.Produto;
 import com.david.cursospring.repositories.CategoriaRepository;
 
 @SpringBootApplication
@@ -24,6 +25,17 @@ public class CursoSpringApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
