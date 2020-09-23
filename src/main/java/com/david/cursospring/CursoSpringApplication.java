6a98c817be67b527d4448ae4402b913704c7c333
DavidLuna0 <ddavidr569@gmail.com>
2020-09-23 16:31:28 -0300

    [Refactor] - Creating Runner class for testing and debugging

--- src/main/java/com/david/cursospring/CursoSpringApplication.java
+++ src/main/java/com/david/cursospring/CursoSpringApplication.java
@@ -1,73 +1,13 @@
 package com.david.cursospring;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.david.cursospring.domain.Categoria;
-import com.david.cursospring.domain.Cidade;
-import com.david.cursospring.domain.Estado;
-import com.david.cursospring.domain.Produto;
-import com.david.cursospring.repositories.CategoriaRepository;
-import com.david.cursospring.repositories.CidadeRepository;
-import com.david.cursospring.repositories.EstadoRepository;
-import com.david.cursospring.repositories.ProdutoRepository;
-
 @SpringBootApplication
-public class CursoSpringApplication implements CommandLineRunner {
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	
-	@Autowired
-	private EstadoRepository estadoRepository;
-	
-	@Autowired
-	private CidadeRepository cidadeRepository;
+public class CursoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informatica");
-		Categoria cat2 = new Categoria(null, "Escritorio");
-		
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "Sao Paulo");
-		
-		Cidade c1 = new Cidade(null, "Uberlandia", est1);
-		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
-		est1.getCidade().addAll(Arrays.asList(c1));
-		est2.getCidade().addAll(Arrays.asList(c2, c3));
-		
-		estadoRepository.saveAll(Arrays.asList(est1, est2));
-		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
-		
 	}
 
 }
