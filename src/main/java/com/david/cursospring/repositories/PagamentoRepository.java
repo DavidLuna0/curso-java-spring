8bc809a1290eee9d95987c239a996c2326e7a6d8
DavidLuna0 <ddavidr569@gmail.com>
2020-09-24 17:45:26 -0300

    [Add] - Creating Pagamento and Pedido repositories

---
+++ src/main/java/com/david/cursospring/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.david.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.david.cursospring.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
