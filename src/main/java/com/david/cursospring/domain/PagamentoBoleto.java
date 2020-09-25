160db61e8acaa02958e20b9da871386a3df2e437
DavidLuna0 <ddavidr569@gmail.com>
2020-09-25 09:26:01 -0300

    [Fix] - Fixing cyclycal reference problems

--- src/main/java/com/david/cursospring/domain/PagamentoBoleto.java
+++ src/main/java/com/david/cursospring/domain/PagamentoBoleto.java
@@ -5,13 +5,16 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.david.cursospring.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class PagamentoBoleto extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
 	private Date dataVencimento;
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
 	private Date dataPagamento;
 	
 	public PagamentoBoleto() {
