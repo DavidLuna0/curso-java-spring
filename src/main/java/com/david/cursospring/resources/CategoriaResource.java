dc4f54bf8d426ffb2d08fa315269155a0d43a3d0
David Luna <david.luna@b2wdgital.com>
2020-12-03 09:47:55 -0300

    chore: Creating categorias post method

--- src/main/java/com/david/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/david/cursospring/resources/CategoriaResource.java
@@ -2,13 +2,13 @@ package com.david.cursospring.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.david.cursospring.domain.Categoria;
 import com.david.cursospring.services.CategoriaService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -21,5 +21,12 @@ public class CategoriaResource {
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
