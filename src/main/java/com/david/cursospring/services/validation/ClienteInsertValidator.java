1b4585ed33c1b03761cf02ca9e73fc93b72fb0d6
David Luna <david.luna@b2wdgital.com>
2020-12-09 09:34:35 -0300

    feat: Duplicated email validation on insert

--- src/main/java/com/david/cursospring/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/david/cursospring/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.david.cursospring.services.validation;
 
+import com.david.cursospring.domain.Cliente;
 import com.david.cursospring.domain.enums.TipoCliente;
 import com.david.cursospring.dto.ClienteNewDTO;
+import com.david.cursospring.repositories.ClienteRepository;
 import com.david.cursospring.resources.exceptions.FieldMessage;
 import com.david.cursospring.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -28,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if(o.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(o.getCpfCnpj())) {
             list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repo.findByEmail(o.getEmail());
+
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         //Percorre a lista de field message e adiciona na lista de erro do framework
